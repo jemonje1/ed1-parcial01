83dd24bf986ba7df01993c44792efa6b2b272295
Javi <jemonje@correo.url.edu.gt>
2025-03-25 08:23:30 -0600

    LeetCode

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -1,9 +1,30 @@
 package ed.lab;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class E01FlattenBT {
 
     public void flatten(TreeNode<Integer> root) {
+        if (root == null)
+            return;
 
+        LinkedList<TreeNode<Integer>> result = new LinkedList<>();
+        Stack<TreeNode<Integer>> stack = new Stack<>();
+        stack.push(root);
+        TreeNode<Integer> node=null;
+        while (!stack.isEmpty()) {
+            TreeNode<Integer> curr = stack.pop();
+            if (node != null) {
+                result.add(node);
+                node.right = curr;
+                node.left = null;
+            }
+            if (curr.right != null)
+                stack.push(curr.right);
+            if (curr.left != null)
+                stack.push(curr.left);
+            node = curr;
+        }
     }
-
 }
